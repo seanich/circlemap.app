561f363be709db7f055b5586054dfc29e8c7d3c1
Sean Nichols <git@seanich.com>
2019-06-21 00:49:35 -0400

    Add keyboard navigation to geocoder

--- src/components/GeocoderInput.vue
+++ src/components/GeocoderInput.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="autocomplete">
+  <div class="autocomplete" v-click-outside="hideResults">
     <input
       type="text"
       v-model="query"
       @input="onChange"
+      @focus="showResults"
       placeholder="Search for a location"
+      v-on:keyup.down="resultDown"
+      v-on:keyup.up="resultUp"
+      v-on:keyup.enter="resultSelect"
     />
-    <ul class="autocomplete-results">
+    <ul class="autocomplete-results" v-if="resultsVisible">
       <li
         v-for="(result, i) in results"
         :key="i"
         class="autocomplete-result"
+        v-bind:class="{ selected: i === selectedResultIndex }"
         v-on:click="locationClick(result)"
       >
         {{ result.displayLabel }}
@@ -22,6 +27,7 @@
 <script lang="ts">
 import { debounce } from "lodash-es";
 import Vue from "vue";
+import ClickOutside from "vue-click-outside";
 
 interface GeocoderResult {
   displayLabel: string;
@@ -56,8 +62,13 @@ export default Vue.extend({
   data() {
     return {
       query: "",
+      resultsVisible: false,
+      selectedResultIndex: false as number | false,
       results: [] as GeocoderResult[]
     };
+  },
+  directives: {
+    ClickOutside
   },
   methods: {
     onChange: debounce(
@@ -65,14 +76,33 @@ export default Vue.extend({
         //@ts-ignore
         this.autocompleteResults(this.query);
       },
-      400,
-      { maxWait: 1000 }
+      300,
+      { maxWait: 600 }
     ),
-    updateGeocoderResults: function(rawResults: google.maps.GeocoderResult[]) {
+    resultDown() {
+      this.selectedResultIndex = this.selectedResultIndex === false ? 0 : (this.selectedResultIndex + 1) % this.results.length;
+    },
+    resultUp() {
+      this.selectedResultIndex = (this.selectedResultIndex === false || this.selectedResultIndex === 0)
+        ? this.results.length - 1
+        : this.selectedResultIndex - 1;
+    },
+    resultSelect() {
+      if (this.selectedResultIndex === false) return; 
+      this.locationClick(this.results[this.selectedResultIndex]);
+    },
+    showResults() {
+      this.resultsVisible = true;
+    },
+    hideResults() {
+      this.resultsVisible = false;
+    },
+    updateGeocoderResults(rawResults: google.maps.GeocoderResult[]) {
       this.results = rawResults.map(res => ({
         displayLabel: res.formatted_address,
         location: res.geometry.location.toJSON()
       }));
+      this.selectedResultIndex = false;
     },
     autocompleteResults(query: string): Promise<void | GeocoderResult[]> {
       return (
@@ -88,6 +118,7 @@ export default Vue.extend({
     locationClick(result: GeocoderResult) {
       this.query = result.displayLabel;
       this.$emit("select-location", result.location);
+      this.resultsVisible = false;
     }
   }
 });
@@ -132,5 +163,9 @@ export default Vue.extend({
 
 .autocomplete-result:hover {
   background-color: #f1f9fe;
+}
+
+.autocomplete-result.selected {
+  background-color: #c4d7e6;
 }
 </style>
